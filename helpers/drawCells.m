5409f6e48a08dfa7bff8914e5795ef3b1c49daf0
Benjamin Walker <ben@benjaminwalker.info>
2022-07-29 10:44:55 +0100

    drawCells now allows line color as an optional second argument.

--- helpers/drawCells.m
+++ helpers/drawCells.m
@@ -1,7 +1,10 @@
-function drawCells(mesh2D)
-    cells = mesh2D.cells;
+function drawCells(mesh2D,color)
+    if nargin < 2
+        color = 'black';
+    end
+    cells = mesh2D.cells(:,1:mesh2D.numCells);
     lookup = mesh2D.lookup;
     X = [cells(lookup.XBoundLower,:);cells(lookup.XBoundLower,:);cells(lookup.XBoundUpper,:);cells(lookup.XBoundUpper,:)];
     Y = [cells(lookup.YBoundLower,:);cells(lookup.YBoundUpper,:);cells(lookup.YBoundUpper,:);cells(lookup.YBoundLower,:)];
-    patch(X,Y,'black','FaceColor','none')
+    patch(X,Y,'black','FaceColor','none','EdgeColor',color)
 end
